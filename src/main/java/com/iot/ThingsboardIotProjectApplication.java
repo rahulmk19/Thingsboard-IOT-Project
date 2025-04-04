9b26b7b874152e4f11eeef54996d16340566283c
RahulMk <rahulmk.94802@gmail.com>
2025-04-04 13:26:27 +0530

    Dokcer compose file added and it is running succesfully

---
+++ src/main/java/com/iot/ThingsboardIotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingsboardIotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThingsboardIotProjectApplication.class, args);
+	}
+
+}
